dad504b2ae2877bf871f45a8197ac747c04562ce
guilhermefrazao <guilhermefrazao@discente.ufg.br>
2026-02-25 18:23:40 -0300

    feat: Simple speech translator
    Criação de um código que consegue traduzir comandos de fala simples para texto e consegue executar eles.

--- speech.py
+++ speech.py
@@ -7,7 +7,10 @@ from interact_app import open_browser, open_app
 
 
 
-logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", level=logging.INFO, encoding="utf-8", 
+logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", 
+                    datefmt="%Y-%m-%d %H:%M:%S",
+                    level=logging.INFO, 
+                    encoding="utf-8", 
                     handlers=[logging.FileHandler("voice.log", encoding="utf-8"),
                               logging.StreamHandler(sys.stdout)])
 
@@ -17,38 +20,36 @@ class Speech_to_text():
         self.recognizer = sr.Recognizer()
         self.recording_time = 30
         self.output_file = "output.txt"
-        self.text_recognized = ""
 
 
     def main(self):
         logging.info("Start recording")
-        start_time = time.perf_counter()
         with sr.Microphone() as microphone:
             self.recognizer.adjust_for_ambient_noise(microphone, duration=1)
             logging.info("Adjusted for ambient noise. Linstening")
-            while time.perf_counter() - start_time < self.recording_time:
-                remaining = self.recording_time - (time.perf_counter() - start_time)
-                logging.info(f"Remaining: {remaining:.2f}")
-                try:
-                    audio = self.recognizer.listen(microphone, timeout=5, phrase_time_limit=10)
-                    logging.info(f"Audio recorded: {audio}")
-                    text_log, text = self.recognize_speech(audio)
-                    self.save_text(text_log)
-                    self.text_recognized += text + " "
-
-                except sr.WaitTimeoutError:
-                    logging.error("Listenting timeout while waiting for a phrase to start")
+            
+            try:
+                audio = self.recognizer.listen(microphone, timeout=20, phrase_time_limit=5)
                 
-                except Exception as e:
-                    logging.error(f"Error {e}")
-                    time.sleep(0.5)
+                text_log, text = self.recognize_speech(audio)
+                
+                self.save_text(text_log)
+
+                self.process_input(text)
+
+            except sr.WaitTimeoutError:
+                logging.error("Listenting timeout while waiting for a phrase to start")
+            
+            except Exception as e:
+                logging.error(f"Error {e}")
+                time.sleep(0.5)
 
         logging.info("Finished recording! Transcription saved to: %s", self.output_file)
 
 
-    def recognize_speech(self, audio):
+    def recognize_speech(self, audio) -> tuple[str, str]:
         try:
-            text = self.recognizer.recognize_whisper(audio, model="base", show_dict=True, language="portuguese")
+            text = self.recognizer.recognize_whisper(audio, model="small", show_dict=True, language="portuguese")
             timestamp = time.strftime("{%H:%M:%S}")
             
             logging.info(f"Recognizing speech: {text}")
@@ -57,9 +58,6 @@ class Speech_to_text():
         
         except sr.UnknownValueError:
             logging.error("Could not understand")
-
-        except sr.RequestError as re:
-            logging.info(f"Speech recognition service error: {re}")
         
         except Exception as e:
             logging.error(f"Error during recognition: {e}")
@@ -71,11 +69,11 @@ class Speech_to_text():
                 f.write(text + '\n')
 
 
-    def process_input(self):
-        if not self.text_recognized:
+    def process_input(self, text: str) -> None:
+        if not text:
             return
     
-        comando = self.text_recognized.lower()
+        comando = text.lower()
         logging.info(f"Analisando o comando: {comando}")
 
         if "abrir" in comando or "abra" in comando:
@@ -106,4 +104,3 @@ class Speech_to_text():
 if __name__ == "__main__":
     audio_trascription = Speech_to_text()
     audio_trascription.main()
-    audio_trascription.process_input()
