3a0c5ae4ce6359389812bb4bc4dd6340390dd7f0
guilhermefrazao <guilhermefrazao@discente.ufg.br>
2026-03-09 10:21:57 -0300

    feat: Desligar computador
    Adicionando uma feature de reconhecimento quando desejo desligar o computador

--- voice/interact_app.py
+++ voice/interact_app.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import subprocess
 
 from AppOpener import open, close
 
@@ -14,9 +15,13 @@ def translation_tasks(text: str) -> None:
 
     padrao_busca_fechar = r"\b(feche|fechar|fecha|fecho|encerre|encerrar|encerra|encerro|pare|parar)\s+(o\s+|a\s+|um\s+|uma\s+)?(.*)"
 
+    padrao_desligar_computador = r"\b(desligar|desligue|desliga)"
+
     resultado_executar = re.search(padrao_busca_executar, comando)
 
     resultado_fechar = re.search(padrao_busca_fechar, comando)
+
+    resultado_desligar = re.search(padrao_desligar_computador, comando)
 
     if resultado_executar:
         nome_do_app = resultado_executar.group(3).strip()
@@ -31,6 +36,9 @@ def translation_tasks(text: str) -> None:
         logging.info(f"Comando identificado! Tentando fechar: {nome_do_app}...")
 
         close_app(nome_do_app)
+
+    elif resultado_desligar:
+        subprocess.run(["shutdown", "/s", "/t", "0"])
         
     else:
         logging.info("Nenhum comando reconhecido nesta frase.")
